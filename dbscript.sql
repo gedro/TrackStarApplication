94d61787fd06aba1c78d316daf11efa0ac7e04c9
Gabor Harsanyi <harsanyigabor@gmail.com>
2012-10-15 05:34:04 +0300

    Fixed sql script and added project table

--- dbscript.sql
+++ dbscript.sql
@@ -1,13 +1,28 @@
 CREATE DATABASE trackstar_dev;
 
-CREATE USER 'trackstar'@'%' IDENTIFIED BY  '';
+CREATE USER 'trackstar'@'%' IDENTIFIED BY  'trackstar';
 
-GRANT USAGE ON * . * TO  'trackstar'@'%' IDENTIFIED BY  '' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+GRANT ALL PRIVILEGES ON * . * TO  'trackstar'@'%'  IDENTIFIED BY  'trackstar' WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
 
 GRANT ALL PRIVILEGES ON  `trackstar_dev` . * TO  'trackstar'@'%';
 
-CREATE USER 'trackstar'@'localhost' IDENTIFIED BY  '***';
+CREATE USER 'trackstar'@'localhost' IDENTIFIED BY  'trackstar';
 
-GRANT ALL PRIVILEGES ON * . * TO  'trackstar'@'localhost' IDENTIFIED BY  '***' WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
+GRANT ALL PRIVILEGES ON * . * TO  'trackstar'@'localhost' IDENTIFIED BY  'trackstar' WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
 
-GRANT ALL PRIVILEGES ON  `trackstar\_%` . * TO  'trackstar'@'localhost';
+GRANT ALL PRIVILEGES ON  `trackstar_dev` . * TO  'trackstar'@'localhost';
+
+FLUSH PRIVILEGES;
+
+USE trackstar_dev;
+
+CREATE TABLE tbl_project (
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(128),
+    description TEXT,
+    create_time DATETIME,
+    create_user_id INTEGER,
+    update_time DATETIME,
+    update_user_id INTEGER
+);
+
