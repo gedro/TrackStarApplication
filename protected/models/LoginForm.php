a2572b48cf17eb54ad4ad4b03b4959260fd218cc
Gabor Harsanyi <harsanyigabor@gmail.com>
2012-10-20 15:53:04 +0300

    Added user last login time functionality

--- protected/models/LoginForm.php
+++ protected/models/LoginForm.php
@@ -60,18 +60,20 @@ class LoginForm extends CFormModel
 	 */
 	public function login()
 	{
-		if($this->_identity===null)
-		{
+		if($this->_identity === null) {
 			$this->_identity=new UserIdentity($this->username,$this->password);
 			$this->_identity->authenticate();
 		}
-		if($this->_identity->errorCode===UserIdentity::ERROR_NONE)
-		{
+		
+		if($this->_identity->errorCode === UserIdentity::ERROR_NONE) {
 			$duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
 			Yii::app()->user->login($this->_identity,$duration);
+			
+			User::model()->updateByPk($this->_identity->id, array('last_login_time' => new CDbExpression('NOW()')));
+			
 			return true;
-		}
-		else
+		} else {
 			return false;
+		}
 	}
 }
