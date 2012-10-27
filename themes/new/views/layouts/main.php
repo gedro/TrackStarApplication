05eebf6b56e1b6853b7c6ff6e81b814c7a17f319
Gabor Harsanyi <harsanyigabor@gmail.com>
2012-10-28 01:09:33 +0300

    Restricting admin access

--- themes/new/views/layouts/main.php
+++ themes/new/views/layouts/main.php
@@ -31,6 +31,8 @@
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>'Admin', 'url'=>array('/admin/default/index'),
+                               'visible' => Yii::app()->authManager->checkAccess("admin", Yii::app()->user->id)),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
