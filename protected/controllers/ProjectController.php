9e7958435b673f8b25a72a3ab9daa064bde124b0
Gabor Harsanyi <harsanyigabor@gmail.com>
2012-10-28 14:56:55 +0200

    Displaying the admin message to users

--- protected/controllers/ProjectController.php
+++ protected/controllers/ProjectController.php
@@ -147,9 +147,21 @@ class ProjectController extends Controller
             'application/rss+xml',
             $this->createUrl('comment/feed')
         );
+        
+        //get the latest system message to display based on the update_time column
+        $sysMessage = SysMessage::model()->find(array(
+            'order'=>'t.update_time DESC',
+        ));
+        
+        if($sysMessage != null) {
+            $message = $sysMessage->message;
+        } else {
+            $message = null;
+        }
 
 		$this->render('index', array(
 			'dataProvider' => $dataProvider,
+			'sysMessage' => $message,
 		));
 	}
 
